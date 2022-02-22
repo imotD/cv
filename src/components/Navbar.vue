4ded4da0fe977fcc4a1a36c914744bc0c9229914
Tommy Alhamra <tommy.alhamra@gmail.com>
2022-02-22 20:46:17 +0700

    fix(navbar): button collapse

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,22 +1,62 @@
 <template>
-  <nav class="flex container mx-auto justify-between items-center py-3">
-    <div>
-      <img width="40" src="../assets/logo.png" alt="" />
-    </div>
-    <div class="text-sm">
-      <span class="px-3 py-2">About</span>
-      <span class="px-3 py-2">Experience</span>
-      <span class="px-3 py-2">Work</span>
-      <span class="px-3 py-2">Contact</span>
-      <a
-        href="../../pdf/CV_Tommy_Alhamra.pdf"
-        download="CV_Tommy_Alhamra.pdf"
-        target="_blank"
+  <nav
+    class="
+      flex flex-col
+      md:flex-row
+      justify-between
+      md:items-center
+      container
+      mx-auto
+      py-3
+      px-5
+    "
+  >
+    <div class="flex justify-between items-center">
+      <div>
+        <img width="40" src="../assets/logo.png" alt="" />
+      </div>
+
+      <button
+        @click.stop="isOpen = !isOpen"
+        class="text-yellow-300 block md:hidden"
       >
-        <button class="px-3 py-2 border border-yellow-300 rounded">
-          Resume
-        </button>
-      </a>
+        <span
+          class="iconify"
+          :data-icon="`mdi:${isOpen ? 'close' : 'menu'}`"
+          data-width="24"
+        ></span>
+      </button>
+    </div>
+
+    <div :class="`${isOpen ? 'block' : 'hidden'} md:block `">
+      <div
+        class="
+          bg-gray-900
+          md:bg-transparent
+          text-sm
+          flex flex-col
+          md:flex-row
+          pt-4
+          mt-2
+          md:mt-0 md:pt-0
+          border-t border-gray-700
+          md:border-t-0
+        "
+      >
+        <a href="#" class="px-3 py-2 block md:inline-block">About</a>
+        <a href="#" class="px-3 py-2 block md:inline-block">Experience</a>
+        <a href="#" class="px-3 py-2 block md:inline-block">Work</a>
+        <a href="#" class="px-3 py-2 block md:inline-block">Contact</a>
+        <a
+          href="../../pdf/CV_Tommy_Alhamra.pdf"
+          download="CV_Tommy_Alhamra.pdf"
+          target="_blank"
+        >
+          <button class="px-3 py-2 border border-yellow-300 rounded">
+            Resume
+          </button>
+        </a>
+      </div>
     </div>
   </nav>
 </template>
@@ -24,6 +64,16 @@
 <script>
 export default {
   name: "Navbar",
+  data() {
+    return {
+      isOpen: false,
+    };
+  },
+  methods: {
+    openMenu() {
+      this.isOpen = true;
+    },
+  },
 };
 </script>
 
